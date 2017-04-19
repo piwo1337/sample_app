0c4f8784e2cb21502d6706462553e3e1240dbf7c
Klemen <piwo.owned@gmail.com>
2017-04-19 17:33:19 +0000

    Layouts and Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/help'
+  get '/signup', to: 'users#new'
+  get '/contact', to: 'static_pages#contact'
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
 
   root 'static_pages#home'
 end
