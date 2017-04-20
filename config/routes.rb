6e2dfc682dab6a95ea39753eba9befc0b68abb7c
Klemen <piwo.owned@gmail.com>
2017-04-20 16:39:20 +0000

    Finish SignUP

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get '/signup', to: 'users#new'
+  root 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
-
-  root 'static_pages#home'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
+  
 end
