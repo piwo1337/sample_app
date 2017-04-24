d33ba47b6fd0765347ad14fc4b0a9ade2535c0a4
Klemen <piwo.owned@gmail.com>
2017-04-24 18:16:04 +0000

    Fix4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
   
   def index
-    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+    @users = User.paginate(page: params[:page])
   end
   
   def show
