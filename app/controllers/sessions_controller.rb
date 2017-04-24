7c5f9f371bbb28a9d25f8ed6e83df3b6fe035661
Klemen <piwo.owned@gmail.com>
2017-04-24 17:32:24 +0000

    Auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,25 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      remember user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      if user.activated?
+        log_in user
+        remember user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
     else
+      message = "Account not activated! "
+      message += "Check your email for activation link! "
+      flash[:warning] = message
+      redirect_to root_url
+    end
+  else
       flash.now[:danger] = 'Invalid Email/Password combination!'
       render 'new'
     end
   end
   
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
   
   def index
-    @users = User.paginate(page: params[:page], per_page: 10).order('name ASC')
+    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
   end
   
   def show
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the SAMPLE APP!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
