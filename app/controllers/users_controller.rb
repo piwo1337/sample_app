61c805417914b0b60b3478747fbd6c8d81847d29
Klemen <piwo.owned@gmail.com>
2017-04-25 16:53:31 +0000

    Micro

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
   
@@ -9,6 +8,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -52,12 +52,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
-  def logged_in_user
-    unless logged_in?
-      flash[:danger] = "Please log in"
-      redirect_to login_url
-    end
-  end
+ 
   
   def correct_user
     @user = User.find(params[:id])
